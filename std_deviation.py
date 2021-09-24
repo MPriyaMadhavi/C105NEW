683adae6c9b22728438cf67857ea2e704e6b3b43
MPriyaMadhavi <70110684+MPriyaMadhavi@users.noreply.github.com>
2021-09-24 18:51:14 +0530

    Add files via upload

---
+++ std_deviation.py
@@ -0,0 +1,33 @@
+
+import math
+
+
+
+# list of elements to calculate mean
+import csv
+with open('data.csv', newline='') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+
+data = file_data[0]
+
+#getting the mean
+# data = [20,69,56,90,40,99,86,100,70,69,80,65,57,82,90,70,79,39,90,80,86,53,97,95,88,47,100,56,97,100] #list of x or y
+# data=[60,61,65,63,98,99,90,95,91,96]
+
+# finding mean
+
+
+
+# squaring and getting the values
+
+
+#getting sum
+
+#dividing the sum by the total values
+
+# getting the deviation by taking square root of the result
+
+
+# print("derived using predefined function ",statistics.stdev(data))
